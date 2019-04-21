001f18dcfd7ccde7692a99fa4c3f3736ca36e4ba
krishnath poologanathan <krishnathp@tradeix.com>
2019-04-21 02:03:39 +0100

    Intial commit WIP

---
+++ camel-k-clients/src/main/kotlin/com/tradeix/camelk/demo/CamelKDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.tradeix.camelk.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CamelKDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CamelKDemoApplication>(*args)
+}
